4df0d55121a18087b389986a26b1d53e059549aa
Zach Morrissey <zachmor05@gmail.com>
2023-12-19 10:37:01 -0800

    Add poetry package mgmt

--- automl.py
+++ automl.py
@@ -1,8 +1,7 @@
-
 # coding: utf-8
 
 # # AutoML Runs Created
-# 
+#
 # This is a model to try to predict how many runs created a specific player will get over the next few weeks.
 
 # In[2]:
@@ -40,20 +39,106 @@ df = pybaseball.batting_stats("2019")
 # choose list of valid columns to keep
 df.columns = [x.lower() for x in df.columns]
 columns_to_keep = [
- 'age','g','ab', 'pa', 'h', '1b', '2b', '3b', 'hr', 'r', 'rbi', 'bb',
- 'ibb', 'so', 'hbp', 'sf', 'sh', 'gdp', 'sb', 'cs', 'avg', 'gb', 'fb',
- 'ld', 'iffb', 'pitches', 'balls', 'strikes', 'ifh', 'bu', 'buh', 'bb%',
- 'k%', 'bb/k', 'obp', 'slg', 'ops', 'iso', 'babip', 'gb/fb', 'ld%', 'gb%',
- 'fb%', 'iffb%', 'hr/fb', 'ifh%', 'buh%', 'woba', 'wraa', 'bat',
- 'rep', 'pos', 'rar', 'war', 'spd', 'wrc+', 'wpa', '-wpa', '+wpa',
- 're24', 'rew', 'pli', 'ph', 'wpa/li', 'fbv', 'wfb', 'wsl',
- 'wct', 'wcb', 'wfb/c', 'wsl/c', 'wct/c', 'wcb/c', 'o-swing%',
- 'z-swing%', 'swing%', 'o-contact%', 'z-contact%',
- 'contact%', 'zone%', 'f-strike%', 'swstr%', 'bsr', 'pace', 'def', 'wsb',
- 'ubr', 'off', 'lg', 'wgdp', 'pull%', 'cent%', 'oppo%', 'soft%',
- 'med%', 'hard%', 'tto%']
+    "age",
+    "g",
+    "ab",
+    "pa",
+    "h",
+    "1b",
+    "2b",
+    "3b",
+    "hr",
+    "r",
+    "rbi",
+    "bb",
+    "ibb",
+    "so",
+    "hbp",
+    "sf",
+    "sh",
+    "gdp",
+    "sb",
+    "cs",
+    "avg",
+    "gb",
+    "fb",
+    "ld",
+    "iffb",
+    "pitches",
+    "balls",
+    "strikes",
+    "ifh",
+    "bu",
+    "buh",
+    "bb%",
+    "k%",
+    "bb/k",
+    "obp",
+    "slg",
+    "ops",
+    "iso",
+    "babip",
+    "gb/fb",
+    "ld%",
+    "gb%",
+    "fb%",
+    "iffb%",
+    "hr/fb",
+    "ifh%",
+    "buh%",
+    "woba",
+    "wraa",
+    "bat",
+    "rep",
+    "pos",
+    "rar",
+    "war",
+    "spd",
+    "wrc+",
+    "wpa",
+    "-wpa",
+    "+wpa",
+    "re24",
+    "rew",
+    "pli",
+    "ph",
+    "wpa/li",
+    "fbv",
+    "wfb",
+    "wsl",
+    "wct",
+    "wcb",
+    "wfb/c",
+    "wsl/c",
+    "wct/c",
+    "wcb/c",
+    "o-swing%",
+    "z-swing%",
+    "swing%",
+    "o-contact%",
+    "z-contact%",
+    "contact%",
+    "zone%",
+    "f-strike%",
+    "swstr%",
+    "bsr",
+    "pace",
+    "def",
+    "wsb",
+    "ubr",
+    "off",
+    "lg",
+    "wgdp",
+    "pull%",
+    "cent%",
+    "oppo%",
+    "soft%",
+    "med%",
+    "hard%",
+    "tto%",
+]
 df = df[columns_to_keep]
-df_y = df['ab'] * df['obp'] * df['slg']
+df_y = df["ab"] * df["obp"] * df["slg"]
 
 
 # In[5]:
@@ -66,5 +151,4 @@ x_train, x_test, y_train, y_test = model_selection.train_test_split(df, df_y)
 # In[1]:
 
 
-regressor.fit(x_train, y_train, dataset_name='boston')
-
+regressor.fit(x_train, y_train, dataset_name="boston")
